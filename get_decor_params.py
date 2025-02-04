e2730c75439b2e62213daf08c6e5c3170b445d90
Kevin Pedro <kpedro88@gmail.com>
2025-02-04 10:46:56 -0600

    add alternate mode of getting decorrelated params

--- get_decor_params.py
+++ get_decor_params.py
@@ -363,14 +363,43 @@ def get_objs(args):
     return w, fit
 
 if __name__=="__main__":
-    parser = argparse.ArgumentParser(description="Compute hessian by hand.")
-    parser.add_argument("-w", "--workspace", metavar="ROOTFILE:WORKSPACE", help="Workspace to load", required=True)
-    parser.add_argument("-f", "--fit", metavar="ROOTFILE:FIT_NAME", help="Fit result to load", required=True)
-    parser.add_argument("-m", "--model", help="Model to load", default="ModelConfig")
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description="Get decorrelated parameter expressions from covariance matrix",
+    )
+    subparsers = parser.add_subparsers()
+
+    p_hessian = subparsers.add_parser('hessian')
+    p_hessian.add_argument("-w", "--workspace", metavar="ROOTFILE:WORKSPACE", help="Workspace to load", required=True)
+    p_hessian.add_argument("-f", "--fit", metavar="ROOTFILE:FIT_NAME", help="Fit result to load", required=True)
+    p_hessian.add_argument("-m", "--model", help="Model to load", default="ModelConfig")
+    p_hessian.set_defaults(mode='hessian')
+
+    p_cov = subparsers.add_parser('cov')
+    p_cov.add_argument("-t", "--tree", metavar="ROOTFILE:TREE_NAME", help="Tree to load", required=True)
+    p_cov.add_argument("-p", "--parameters", nargs='+', help="Parameters to use", required=True)
+    p_cov.set_defaults(mode='cov')
+
     args = parser.parse_args()
 
-    w, fit = get_objs(args)
+    if args.mode=='hessian':
+        w, fit = get_objs(args)
 
-    param_names = [p.GetName() for p in fit.floatParsFinal() if p.GetName().startswith("bsvj")]
-    decoVector = DecorrelatedNuisanceVector.fromRooFitResult("@", fit, param_names)
+        param_names = [p.GetName() for p in fit.floatParsFinal() if p.GetName().startswith("bsvj")]
+        decoVector = DecorrelatedNuisanceVector.fromRooFitResult("@", fit, param_names)
+    elif args.mode=='cov':
+        import uproot as up
+        fname, tname = args.tree.split(':')
+        f = up.open(fname)
+        pnames = args.parameters
+        params = f[tname].arrays(pnames)
+
+        # compute covariance matrix from scan in limit tree
+        X = np.asarray([params[p] for p in pnames])
+        means = np.mean(X, axis=1)
+        cov = np.cov(X)
+        decoVector = DecorrelatedNuisanceVector("@", means, cov)
+
+    # output
     print(decoVector.correlated_str)
+
