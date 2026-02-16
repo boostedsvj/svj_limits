b14a97d735be4773f8bbd7302597a7f54da6d334
Kevin Pedro <kpedro88@gmail.com>
2026-02-16 17:35:20 -0600

    add some missing debug printouts

--- quick_plot.py
+++ quick_plot.py
@@ -1270,6 +1270,8 @@ def bkgtf():
             tf_post['arr'] = bsvj.th1_to_hist(tf_post['th1'])
             if verbose: print('tf_post_th1', tf_post['arr']['vals'].tolist())
             fit_post = get_tf_fit(fitresult_mc, 'tf_mc', tf_post['th1'], mt['scaled'], tf_post['bkg_eff'], basis=basis_mc)
+            if verbose: print('fit_post', fit_post['tf_fn_vals'].tolist())
+            if verbose: print('chi2_post', fit_post['chi2'], fit_post['ndf'])
             plot_tf(outfile, mt, tf_post, fit_post, ylabel=f'$TF_{{\\mathrm{{MC}}}}^{{\\mathrm{{postfit}}}}$ ({regions[0]} / {regions[1]})', suff='mcpost', title=title, canvas=mc_canvas)
             # plot_tf(outfile, mt, tf_post, fit_post, ylabel=f'$TF_{{\\mathrm{{MC}}}}^{{\\mathrm{{postfit}}}}$ ({regions[0]} / {regions[1]})', suff='mcpost', title=title)
         else:
